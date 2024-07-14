6f0f01c527065f6f0b5a7cd388a08a2e14b3555e
Clayton Pereira <cpereiramt@gmail.com>
2024-07-14 17:55:11 -0400

    feat: updating account-overview component

--- src/account-overview.jsx
+++ src/account-overview.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHardHat } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
+import { PropTypes } from "prop-types";
 
-import './account-overview.css';
+const AccountOverviewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
 
 export const AccountOverview = ({data}) => {
   console.log(data);
 
   return (
-    <div className="AccountOverview">
-      <FontAwesomeIcon icon={faHardHat} />
-      Build your components here
-    </div>
+    <AccountOverviewWrapper>
+
+      </AccountOverviewWrapper>
   )
 }
+AccountOverview.propTypes = {
+  data: PropTypes.shape({
+    support: PropTypes.object.isRequired,
+    sales: PropTypes.object.isRequired,
+  }).isRequired,
+};
 
 export default AccountOverview;
